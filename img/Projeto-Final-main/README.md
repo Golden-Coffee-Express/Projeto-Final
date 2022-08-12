6226a06b370c25d22270cb516ac0d7c86df49e96
DevDanielDeSouza <danclagab@gmail.com>
2022-08-12 09:35:47 -0300

    Logado e Deslogado
    Adicionando Logado e Deslogado

--- img/Projeto-Final-main/README.md
+++
@@ -1,2 +0,0 @@
-# Projeto-Final
-Golden Express Coffee 
--- img/Projeto-Final-main/php/erros.php
+++
@@ -1,7 +0,0 @@
-<?php  if (count($erros) > 0) :  /* Este arquivos imprime os erros */ ?>
-  <div class="erro">
-  	<?php foreach ($erros as $erro) : ?>
-  	  <p><?php echo $erro ?></p>
-  	<?php endforeach ?>
-  </div>
-<?php  endif ?>
